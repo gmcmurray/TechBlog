9889b3febbea579f9edef219e9a8efc840237cf1
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-08-19 04:21:09 -0700

    fixed bug with naming convention in signupForm to create post for username

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -54,7 +54,7 @@ router.post('/', async (req, res) => {
 
   try {
       const userData = await User.create({
-        name: req.body.name,
+        name: req.body.username,
         email: req.body.email,
         password: req.body.password});
 
