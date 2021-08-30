9dabeb5c3dcc3e833335192d0d52c6f8344b2b80
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-08-29 22:23:35 -0700

    making layout responsive to mobile

--- public/js/blog-dash.js
+++ public/js/blog-dash.js
@@ -13,34 +13,3 @@
    document.querySelector(".btndel").addEventListener('click',
    (event) => console.log(event.target));
 
-  async function DeleteIt(pkid) {
-    try {
-      const response = await fetch(`/api/blogposts/${pkid}`, { method: 'DELETE' });
-      if (response.ok) {
-        document.location.reload();
-      }
-    }
-    catch (err) { console.log('Not deleted, not found') }
-  }
-
-  async function updateBlog(idd) {
-    try {
-      const title = document.querySelector(`#blogtitle${idd}`).value.trim();
-      const content = document.querySelector(`#blogcontent${idd}`).value.trim();
-      console.log(content)
-      if (content) {
-        const response = await fetch(`/api/blogposts/${idd}`, {
-          method: 'PUT',
-          body: JSON.stringify({ title, content }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-          document.location.reload();
-        }
-      }
-    }
-    catch (err) {
-      console.log('Failed to edit comment.')
-    }
-  };
